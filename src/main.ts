0e498f42d9de6da5292d360a0ead06ef0a368016
randomizednum <101424255+randomizednum@users.noreply.github.com>
2024-10-29 19:27:15 +0300

    Rewrite main.ts and related scripts to use serveFile

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { loadApi, processApiRequest } from "./api.ts";
-import { serveFile } from "./cdn.ts";
+import { serveRequest } from "./cdn.ts";
 import { DB, initDefaultTables, loadDefaultDB } from "./db.ts";
-import { getHtml, loadDynamicPages, parseHtml } from "./html.ts";
+import { serveHtml, loadDynamicPages, parseHtml } from "./html.ts";
 
 const KILL_SWITCH = false;
 
@@ -29,8 +29,9 @@ export async function serve(): Promise<Deno.HttpServer<Deno.NetAddr>> {
         console.log(`${(now.toUTCString())} @${(now.getMilliseconds() / 1e3).toFixed(3)} | ${req.method} ${info.remoteAddr.hostname}:${info.remoteAddr.port} -> ${req.url}`);
 
         const url = new URL(req.url);
+        const path = url.pathname;
 
-        if (url.pathname.startsWith("/info") || rickrollDetector.test(url.pathname) || KILL_SWITCH)
+        if (path.startsWith("/info") || rickrollDetector.test(url.pathname) || KILL_SWITCH)
             return new Response(null, {
                 status: 302,
                 headers: {
@@ -38,47 +39,16 @@ export async function serve(): Promise<Deno.HttpServer<Deno.NetAddr>> {
                 }
             });
 
-        if (url.pathname.startsWith("/api"))
+        if (path.startsWith("/api"))
             return await processApiRequest(req, url);
 
-        if (url.pathname.startsWith("/cdn"))
-            return serveFile(req, url.pathname.replace(/^\/cdn/g, ""));
-
-        if (url.pathname.startsWith("/asset"))
-            return serveFile(req, url.pathname.replace(/^\/asset/g, ""), "./asset");
-
-        if (url.pathname.startsWith("/styles")) {
-            if (url.pathname !== url.pathname.replace(/\.\.\/|\/\.\./g, ""))
-                return new Response("Forbidden", { status: 403 });
-
-            try {
-                const content = await Deno.readFile(`.${url.pathname}`);
-                return new Response(content, { status: 200, headers: { "Content-Type": "text/css" }});
-            } catch (_e) {
-                return new Response("Not found", { status: 404 });
-            }
-        }
-
-        if (url.pathname.startsWith("/scripts")) {
-            if (url.pathname !== url.pathname.replace(/\.\.\/|\/\.\./g, ""))
-                return new Response("Forbidden", { status: 403 });
-
-            try {
-                const content = await Deno.readFile(`.${url.pathname}`);
-                return new Response(content, { status: 200, headers: { "Content-Type": "application/javascript" }});
-            } catch (_e) {
-                return new Response("Not found", { status: 404 });
-            }
-        }
+        if (path.startsWith("/cdn") || path.startsWith("/asset") || path.startsWith("/styles") || path.startsWith("/scripts"))
+            return await serveRequest(req, ".");
 
         if (dynamicPages[url.pathname] !== undefined)
             return await dynamicPages[url.pathname].exec(req);
 
-        return new Response(getHtml(url.pathname), {
-            headers: {
-                "Content-Type": "text/html; charset=utf-8"
-            }
-        });
+        return await serveHtml(url.pathname);
     })
 }
 
