0a816312290f92cd8b377d2a68574f3f4a8e7366
tikoko23 <code.tikoko@gmail.com>
2024-10-30 19:36:50 +0300

    added client-side event api

---
+++ scripts/websocket.js
@@ -0,0 +1,50 @@
+import { createMessageHolder } from "./message-ui.js";
+import { getMessageHolder } from "./message-ui.js";
+import { addMessage } from "./message-ui.js";
+
+const SOCKET_URL = `/socket`;
+
+/**
+ * Creates a WebSocket with a wrapper function to parse messages
+ * @param {string} token
+ * @param {(type: string, data: object) => void} eventHandler
+ * @returns {WebSocket}
+ */
+export function getWebSocket(token, eventHandler) {
+    const socket = new WebSocket(`${SOCKET_URL}?token=${token}`);
+
+    if (typeof eventHandler === "function") {
+        socket.addEventListener("message", event => {
+            let message;
+            try {
+                message = JSON.parse(event.data);
+            } catch (e) {
+                console.error(e);
+                return;
+            }
+
+            eventHandler(message.type, message.object);
+        });
+    }
+
+    return socket;
+}
+
+export async function setupClientListener(token) {
+    token = await token;
+
+    return getWebSocket(token, (type, data) => {
+        switch (type) {
+            case "message.create": {
+                const sender = data.author.name;
+                const content = data.content.body;
+                const id = data.id;
+
+                const messageHolder = getMessageHolder(data.group.id) ?? createMessageHolder(data.group.id);
+
+                addMessage(sender, content, id, messageHolder);
+                break;
+            }
+        }
+    })
+}
