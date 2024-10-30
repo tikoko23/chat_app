3f475c68bca2f7a0a5b47c193131ff0ae01d15cf
tikoko23 <code.tikoko@gmail.com>
2024-10-30 21:04:36 +0300

    added very simple ui messaging

---
+++ scripts/app-message.js
@@ -0,0 +1,28 @@
+import { ENDPOINTS, fetchJSON } from "./api-endpoints.js";
+
+/**
+ * Sends a message and returns the response (This will throw if sending fails!)
+ * @param {string} token
+ * @param {number} groupId
+ * @param {{body: string}} messageContent
+ * @param {string[]|undefined} attachments
+ * @param {number} replyTo
+ * @returns {ResponseMessage}
+ */
+export async function sendMessage(token, groupId, messageContent, attachments = undefined, replyTo = undefined) {
+    const result = await fetchJSON(`${ENDPOINTS.message}/create`, {
+        method: "POST",
+        headers: { "Authorization": token },
+        body: JSON.stringify({
+            messageContent: messageContent,
+            groupId: groupId,
+            replyTo: replyTo,
+            attachments: attachments
+        })
+    });
+
+    if (result.response.status !== 201)
+        throw new Error(`Sending message failed: ${result.body}`);
+
+    return result.obj;
+}
